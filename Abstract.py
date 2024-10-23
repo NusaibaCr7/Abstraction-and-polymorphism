b9da6979414eee800ceebde6638bb70b87dabb8c
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-10-23 18:25:52 +0600

    first commit

---
+++ Abstract.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class hiddenClass(ABC):
+    
+    def print(self,x):
+        print("Passed value: ",x)
+        
+    @abstractmethod
+    def task(self):
+        print("We are inside Abstract task")
+        
+class testClass(hiddenClass):
+    
+    def task(self):
+        print("We are inside test_class task")
+        
+obj1 = testClass()
+obj1.task()
+obj1.print(100)
+
+hidden = hiddenClass()
+hidden.task
